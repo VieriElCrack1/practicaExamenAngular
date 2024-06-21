eff329095f3c4ee6c58e4e0f83e09de81fb2f01e
VieriElCrack1 <santosvieri9@gmail.com>
2024-06-21 18:21:45 -0500

    terminado rutas

---
+++ src/app/not-fund/not-fund.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-not-fund',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-fund.component.html',
+  styleUrl: './not-fund.component.css'
+})
+export class NotFundComponent {
+
+}
